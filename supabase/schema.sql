47ff6112e3499a96b28a01a452d0a9480bf08da6
Ali Baghirzada <bagirzadeh295@gmail.com>
2026-06-26 14:06:53 +0400

    Add files via upload

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -52,6 +52,7 @@ BEGIN
 END;
 $$;
 
+DROP TRIGGER IF EXISTS trg_users_updated_at ON users;
 CREATE TRIGGER trg_users_updated_at
 BEFORE UPDATE ON users
 FOR EACH ROW EXECUTE FUNCTION update_updated_at();
